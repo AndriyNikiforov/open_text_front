8bb57295545dddaefce35a1da0f7e258d3988ea5
AndrewNikiforov <andriy.nikiforov.general@gmail.com>
2020-02-11 17:46:06 +0200

    Change webpack config. Created simple component for testing server work. Created index.html file. Created favicon.ico file.

--- server.js
+++ server.js
@@ -7,5 +7,6 @@ const app = express();
 app.use(express.static(join(__dirname, 'build')));
 
 app.get('/', (req, res) => res.sendFile(join(__dirname, 'build', 'index.html')));
+app.get('/favicon.ico', (req, res) => res.sendFile(join(__dirname, 'public', 'favicon.ico')));
 
 app.listen(9000, () => log('http://localhost:9000'));
